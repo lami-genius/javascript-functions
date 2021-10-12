e9ab4cc107c8bbbeaf821378556b82226b3486aa
F-B-Lami <fonyuyborislami@gmail.com>
2021-10-12 22:38:17 +0200

    bug fix in contains function

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -16,7 +16,7 @@ function same([x, y], [j, k]) {
 
 // The game state to search for `cell` is passed as the `this` value of the function.
 function contains(cell) {
-  let check = (someCell) => some(someCell, cell)
+  let check = (someCell) => same(someCell, cell);
   return this.some(check);
 }
 
