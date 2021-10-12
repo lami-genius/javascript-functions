d920eb118617b1c9892b43e25928e618335dc7d3
F-B-Lami <fonyuyborislami@gmail.com>
2021-10-12 22:44:32 +0200

    printCell complete and tested

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -20,7 +20,12 @@ function contains(cell) {
   return this.some(check);
 }
 
-const printCell = (cell, state) => {};
+const printCell = (cell, state) => {
+  if (contains.call(state, cell))
+    return '\u25A3';
+  else 
+    return '\u25A2';
+};
 
 const corners = (state = []) => {};
 
