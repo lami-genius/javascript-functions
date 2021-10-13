60f82614dd8cdaa03c03ef72f45664fb14006cb8
F-B-Lami <fonyuyborislami@gmail.com>
2021-10-13 02:36:54 +0200

    corners implementation

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,15 +1,13 @@
 function seed() {
-let arr = []
-for (let i = 0; i < arguments.length; i++)
-{
-  arr[i] = arguments[i];
-}
-return arr;
+  let arr = [];
+  for (let i = 0; i < arguments.length; i++) {
+    arr[i] = arguments[i];
+  }
+  return arr;
 }
 
 function same([x, y], [j, k]) {
-  if ((x == j) && (y == k))
-    return true;
+  if (x == j && y == k) return true;
 
   return false;
 }
@@ -21,13 +19,26 @@ function contains(cell) {
 }
 
 const printCell = (cell, state) => {
-  if (contains.call(state, cell))
-    return '\u25A3';
-  else 
-    return '\u25A2';
+  if (contains.call(state, cell)) return "\u25A3";
+  else return "\u25A2";
 };
 
-const corners = (state = []) => {};
+const corners = (state = []) => {
+  if (state.length === 0) {
+    return {
+      topRight: [0, 0],
+      bottomLeft: [0, 0],
+    };
+  }
+
+  const xs = state.map(([x, _]) => x);
+  const ys = state.map(([_, y]) => y);
+
+  return {
+    topRight: [Math.max(...xs), Math.max(...ys)],
+    bottomRight: [Math.min(...xs), Math.min(...ys)],
+  };
+};
 
 const printCells = (state) => {};
 
@@ -44,53 +55,53 @@ const iterate = (state, iterations) => {};
 const main = (pattern, iterations) => {};
 
 const startPatterns = {
-    rpentomino: [
-      [3, 2],
-      [2, 3],
-      [3, 3],
-      [3, 4],
-      [4, 4]
-    ],
-    glider: [
-      [-2, -2],
-      [-1, -2],
-      [-2, -1],
-      [-1, -1],
-      [1, 1],
-      [2, 1],
-      [3, 1],
-      [3, 2],
-      [2, 3]
-    ],
-    square: [
-      [1, 1],
-      [2, 1],
-      [1, 2],
-      [2, 2]
-    ]
-  };
-  
-  const [pattern, iterations] = process.argv.slice(2);
-  const runAsScript = require.main === module;
-  
-  if (runAsScript) {
-    if (startPatterns[pattern] && !isNaN(parseInt(iterations))) {
-      main(pattern, parseInt(iterations));
-    } else {
-      console.log("Usage: node js/gameoflife.js rpentomino 50");
-    }
+  rpentomino: [
+    [3, 2],
+    [2, 3],
+    [3, 3],
+    [3, 4],
+    [4, 4],
+  ],
+  glider: [
+    [-2, -2],
+    [-1, -2],
+    [-2, -1],
+    [-1, -1],
+    [1, 1],
+    [2, 1],
+    [3, 1],
+    [3, 2],
+    [2, 3],
+  ],
+  square: [
+    [1, 1],
+    [2, 1],
+    [1, 2],
+    [2, 2],
+  ],
+};
+
+const [pattern, iterations] = process.argv.slice(2);
+const runAsScript = require.main === module;
+
+if (runAsScript) {
+  if (startPatterns[pattern] && !isNaN(parseInt(iterations))) {
+    main(pattern, parseInt(iterations));
+  } else {
+    console.log("Usage: node js/gameoflife.js rpentomino 50");
   }
-  
-  exports.seed = seed;
-  exports.same = same;
-  exports.contains = contains;
-  exports.getNeighborsOf = getNeighborsOf;
-  exports.getLivingNeighbors = getLivingNeighbors;
-  exports.willBeAlive = willBeAlive;
-  exports.corners = corners;
-  exports.calculateNext = calculateNext;
-  exports.printCell = printCell;
-  exports.printCells = printCells;
-  exports.startPatterns = startPatterns;
-  exports.iterate = iterate;
-  exports.main = main;
+}
+
+exports.seed = seed;
+exports.same = same;
+exports.contains = contains;
+exports.getNeighborsOf = getNeighborsOf;
+exports.getLivingNeighbors = getLivingNeighbors;
+exports.willBeAlive = willBeAlive;
+exports.corners = corners;
+exports.calculateNext = calculateNext;
+exports.printCell = printCell;
+exports.printCells = printCells;
+exports.startPatterns = startPatterns;
+exports.iterate = iterate;
+exports.main = main;
